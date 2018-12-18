6cabdd41a3d0fe355d450800e3a400cd74ad9f96
sieradz <sieradz628@gmail.com>
2018-12-18 22:43:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("icon").onclick = () => {
+    document.getElementById("icon").classList.toggle("iconHamburger");
+    document.getElementById("header").classList.toggle("headerHamburger");
+    document.getElementById("menu").classList.toggle("menuHamburger");
+}
